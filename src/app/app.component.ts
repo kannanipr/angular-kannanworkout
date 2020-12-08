c9841e87cccd42d1168e882cc6dabee21ca67826
kannanipr <kannanipr@gmail.com>
2020-12-08 21:19:09 +0300

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DataService } from "./Services/data.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'kannan';
+export class AppComponent {
+  name = "kannan";
   age = 26;
+
+  constructor(private dataService: DataService) {}
+
+  increaseCount() {
+    this.dataService.count++;
+  }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { DataService } from "./Services/data.service";
 
 @Component({
-  selector: 'hello',
-  template: `<h1> {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  template: `
+    <h1>{{ name }}!</h1>
+    {{ dataService.count }}
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ],
+  providers: [DataService]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+  constructor(private dataService: DataService) {}
 }
